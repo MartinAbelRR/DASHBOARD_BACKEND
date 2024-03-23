bb16009181b4b4b8c643988ab8109824d99d685e
Martin Reyes <martin.reyes@unmsm.edu.pe>
2024-03-23 17:32:28 -0500

    setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -141,12 +141,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dist/static')
-]
-
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
